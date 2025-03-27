e1a5a96900dedd9c5ffaae03cd0a9f8d98256c55
fede altava <federico.altavaredondo@student.icareweb.com>
2025-03-27 14:46:42 +0100

    async-await

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
 import "./App.css";
 
 function App() {
+  const getImage = async () => {
+    try {
+      const apiKey = "ItQAYIvmB678mStqVYjvFBjzaUXR05Jy";
+      const response = await fetch(
+        `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`
+      );
+      const { data } = await response.json();
+      const { url } = data.images.original;
+      const img = document.createElement("img");
+      img.src = url;
+      document.body.append(img);
+      
+    } catch (error) {
+      console.error(error)
+    }
+  };
 
+  getImage();
   return <></>;
 }
+
+// peticion
+//   .then((resp) => {
+//     resp.json().then(({ data }) => {
+//       ;
+
+//     });
+//   })
+//   .catch(console.warn);
 
 export default App;
