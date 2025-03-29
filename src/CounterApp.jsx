f3e8e5d5c8268fc010efd407a8b331f2987b10c0
Federico <fedealtava1992@gmail.com>
2025-03-29 17:30:42 +0100

    useState

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,16 +1,22 @@
-import PropTypes from 'prop-types'
-import React from 'react'
+import PropTypes from "prop-types";
+import React from "react";
+import { useState } from "react";
 
-export const CounterApp = ({value}) => {
+export const CounterApp = ({ value }) => {
+    const [valor, setValor] = useState(value)
+    const handleClick = () => {
+    setValor((prev)=>prev+1);
+  };
+
   return (
     <>
-    <h1>CounterApp</h1>
-    <h2>{value}</h2>
+      <h1>CounterApp</h1>
+      <h2> {valor} </h2>
+      <button onClick={() => handleClick()}>+1</button>
     </>
-  )
-}
-
+  );
+};
 
 CounterApp.prototype = {
-    value: PropTypes.number
-}
+  value: PropTypes.number,
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {App} from './App'
-import FirstApp from './FirstApp'
+import {FirstApp} from './FirstApp'
 import './styles.css'
 import { CounterApp } from './CounterApp'
 
