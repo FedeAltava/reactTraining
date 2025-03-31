8314e6995516024909bcb1fe5c6c464d55fd5314
fede altava <federico.altavaredondo@student.icareweb.com>
2025-03-31 09:18:07 +0200

    working

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -3,16 +3,24 @@ import React from "react";
 import { useState } from "react";
 
 export const CounterApp = ({ value }) => {
-    const [valor, setValor] = useState(value)
-    const handleClick = () => {
-    setValor((prev)=>prev+1);
+  const [valor, setValor] = useState(value);
+  const handleAdd = () => {
+    setValor((prev) => prev + 1);
+  };
+  const handleSub = () => {
+    setValor((prev) => prev - 1);
+  };
+  const handleReset = () => {
+    setValor(value);
   };
 
   return (
     <>
       <h1>CounterApp</h1>
       <h2> {valor} </h2>
-      <button onClick={() => handleClick()}>+1</button>
+      <button onClick={() => handleAdd()}>+1</button>
+      <button onClick={() => handleSub()}>-1</button>
+      <button onClick={() => handleReset()}>reset</button>
     </>
   );
 };
