2ac14df6b7ea5918c423c5c1912a08a806fda599
fede altava <federico.altavaredondo@student.icareweb.com>
2025-03-31 09:37:21 +0200

    base-pruebas add

--- src/bases/01-import.js
+++
@@ -1,8 +0,0 @@
-import heroes from "./heroes";
-
-export const getHeroeById = (id)=> heroes.find((heroe)=>heroe.id===id);
-
-export const getHeroesByOwner = ( owner ) => heroes.filter((heroes)=>heroes.owner===owner )
-
-// console.log(getHeroeById(2));
-// console.log(getHeroesByOwner('Marvel'));
