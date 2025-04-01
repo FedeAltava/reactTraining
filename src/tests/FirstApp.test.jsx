824f3f770a095813dcdca38e4b24a060f2d3c7b8
fede altava <federico.altavaredondo@student.icareweb.com>
2025-04-01 16:21:19 +0200

    test for h1 on first app

--- src/tests/FirstApp.test.jsx
+++ src/tests/FirstApp.test.jsx
@@ -1,10 +1,24 @@
 
-import { describe, test,} from "@jest/globals";
+import { describe, test, expect} from "@jest/globals";
 import { render } from "@testing-library/react";
 import { FirstApp } from "../FirstApp";
 
 describe('FirstApp', ()=>{
-    test('should do match with snapshot',()=>{
-        render(<FirstApp />)
+    test('should match with snapshot',()=>{
+        const title = 'eyyyyyyy';
+        const subtitle = 'yooooo';
+        const name = 'ELPepe';
+        const {container} = render(<FirstApp title={title} subtitle={subtitle} name={name}/>);
+
+        expect(container).toMatchSnapshot();
+
+    });
+    test('should show title as h1',()=>{
+        const title = 'eyyyyyyy';
+        const {container, getByText} = render(<FirstApp title={title} />);
+        const h1 = container.querySelector('h1');
+        getByText(title)
+        expect(h1.innerHTML).toContain(title);
+
     });
 });
