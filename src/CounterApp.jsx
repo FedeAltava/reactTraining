dcad88f54deb770c24ee96833f69972530d6abd4
Federico <fedealtava1992@gmail.com>
2025-04-01 19:09:49 +0200

    ss

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -3,15 +3,15 @@ import React from "react";
 import { useState } from "react";
 
 export const CounterApp = ({ value }) => {
-    const [valor, setValor] = useState(value)
+    const [counter, setCounter] = useState(value)
     const handleClick = () => {
-    setValor((prev)=>prev+1);
+        setCounter(( prev ) => prev + 1);
   };
 
   return (
     <>
       <h1>CounterApp</h1>
-      <h2> {valor} </h2>
+      <h2> {counter} </h2>
       <button onClick={() => handleClick()}>+1</button>
     </>
   );
